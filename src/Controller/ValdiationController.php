88bcdfef755aa4983cd8695782ecc3cd0a1bb3b1
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-01-20 14:13:49 +0000

    MAJ3

--- src/Controller/ValdiationController.php
+++ src/Controller/ValdiationController.php
@@ -105,11 +105,11 @@ class ValdiationController extends AbstractController
 		$result =  file_get_contents('https://api-checkout.cinetpay.com/v2/payment', false, $context);
 		$donnee = json_decode($result);
 		
-		if ($donnee->code === '201'){ dd($donnee);
+		if ($donnee->code === '201'){ //dd($donnee);
 			$data = [
-				'api_response_id' => $donnee.api_response_id,
-                'token' => $donnee.data.payment_token,
-                'url' => $donnee.data.payment_url,
+				'api_response_id' => $donnee->api_response_id,
+                'token' => $donnee->data->payment_token,
+                'url' => $donnee->data->payment_url,
                 'candidater' => $candidater->getId()
 			];
 			
